12f3500a51b3c16350d6165ff49448d70cfd025e
matidominati <mateuszdermont@gmail.com>
2023-09-07 17:33:52 +0200

    MED-02 Patient Model

---
+++ src/main/java/com/github/matidominati/medicalclinic/personInfo/Person.java
@@ -0,0 +1,13 @@
+package com.github.matidominati.medicalclinic.personInfo;
+
+import java.time.OffsetDateTime;
+
+public class Person {
+    private String email;
+    private String password;
+    private long idCardNo;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private OffsetDateTime brithday;
+}
