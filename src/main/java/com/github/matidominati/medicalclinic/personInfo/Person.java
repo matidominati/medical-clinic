0ad16b98cd62823bce91c448a9ff317c1a8938ad
matidominati <mateuszdermont@gmail.com>
2023-09-11 01:45:42 +0200

    MED-02 Patient Model corrected

--- src/main/java/com/github/matidominati/medicalclinic/personInfo/Person.java
+++ src/main/java/com/github/matidominati/medicalclinic/personInfo/Person.java
@@ -1,13 +1,81 @@
 package com.github.matidominati.medicalclinic.personInfo;
 
+import java.time.LocalDate;
 import java.time.OffsetDateTime;
 
 public class Person {
     private String email;
     private String password;
-    private long idCardNo;
+    private String idCardNo;
     private String firstName;
     private String lastName;
     private String phoneNumber;
-    private OffsetDateTime brithday;
+    private LocalDate birthday;
+
+    public Person(String email, String password, String idCardNo, String firstName, String lastName,
+                  String phoneNumber, LocalDate birthday) {
+        this.email = email;
+        this.password = password;
+        this.idCardNo = idCardNo;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.birthday = birthday;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getIdCardNo() {
+        return idCardNo;
+    }
+
+    public void setIdCardNo(String idCardNo) {
+        this.idCardNo = idCardNo;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
 }
