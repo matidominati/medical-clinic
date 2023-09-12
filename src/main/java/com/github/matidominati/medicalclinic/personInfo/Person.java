6aac0f6f998388afc0f312a843d60fab6e133fc4
matidominati <mateuszdermont@gmail.com>
2023-09-12 18:14:21 +0200

    MED-02 Patient Model lombok

--- src/main/java/com/github/matidominati/medicalclinic/personInfo/Person.java
+++ src/main/java/com/github/matidominati/medicalclinic/personInfo/Person.java
@@ -1,81 +1,21 @@
 package com.github.matidominati.medicalclinic.personInfo;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import java.time.LocalDate;
-import java.time.OffsetDateTime;
 
+
+@Data
+@AllArgsConstructor
+@Builder
 public class Person {
     private String email;
     private String password;
-    private String idCardNo;
+    private final String idCardNo;
     private String firstName;
     private String lastName;
     private String phoneNumber;
-    private LocalDate birthday;
+    private final LocalDate birthday;
 
-    public Person(String email, String password, String idCardNo, String firstName, String lastName,
-                  String phoneNumber, LocalDate birthday) {
-        this.email = email;
-        this.password = password;
-        this.idCardNo = idCardNo;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.phoneNumber = phoneNumber;
-        this.birthday = birthday;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getIdCardNo() {
-        return idCardNo;
-    }
-
-    public void setIdCardNo(String idCardNo) {
-        this.idCardNo = idCardNo;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public LocalDate getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(LocalDate birthday) {
-        this.birthday = birthday;
-    }
 }
