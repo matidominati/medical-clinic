90813559602bd216b8e54124d2773963e09abbf0
matidominati <mateuszdermont@gmail.com>
2023-09-14 18:32:42 +0200

    MED-03 Patient Model methods v3

--- src/main/java/com/github/matidominati/medicalclinic/personInfo/PatientController.java
+++ src/main/java/com/github/matidominati/medicalclinic/personInfo/PatientController.java
@@ -1,54 +1,70 @@
 package com.github.matidominati.medicalclinic.personInfo;
 
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
-@Data
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/patients")
+
 public class PatientController {
     private final List<Patient> patients;
+
     @GetMapping
     public List<Patient> getAllPatients() {
         return patients;
     }
+
     @GetMapping("/{email}")
-    public Patient getPatient (@PathVariable String email) {
-        return checkIfPatientExist(email);
+    public Patient getPatient(@PathVariable String email) {
+        Optional<Patient> patient = findPatientByEmail(email);
+        if (patient.isEmpty()) {
+            throw new RuntimeException("Patient with the provied email does not exist");
+        }
+        return patient.get();
     }
+
     @PostMapping
     public Patient addPatient(@RequestBody Patient patient) {
-        if (!patients.contains(patient) && patient.getEmail() != null) {
-            patients.add(patient);
-            return patient;
-        } else {
-            throw new RuntimeException("Pacjent już istnieje w bazie");
+        if (patient.getEmail() == null) {
+            throw new IllegalArgumentException("Email cannot be null");
         }
+        if (patients.contains(patient)) {
+            throw new RuntimeException("The patient already exists in the database");
+        }
+        patients.add(patient);
+        return patient;
     }
+
     @DeleteMapping("/{email}")
     public void deletePatient(@PathVariable String email) {
-        Patient patientToDelete = checkIfPatientExist(email);
-        if (patientToDelete != null) {
-            patients.remove(patientToDelete);
+        Optional<Patient> patientToDelete = findPatientByEmail(email);
+        if (patientToDelete.isPresent()) {
+            patients.remove(patientToDelete.get());
         } else {
-            throw new RuntimeException("Pacjent o podanym email nie istnieje w bazie");
+            throw new RuntimeException("The patient with the given email address does not exist in the database");
         }
     }
+
     @PutMapping("/{email}")
     public void updatePatient(@PathVariable String email, @RequestBody Patient updatedPatient) {
-        Patient patientToUpdate = checkIfPatientExist(email);
-        patientToUpdate.setPassword(updatedPatient.getPassword());
-        patientToUpdate.setFirstName(patientToUpdate.getFirstName());
-        patientToUpdate.setLastName(updatedPatient.getLastName());
-        patientToUpdate.setPhoneNumber(updatedPatient.getPhoneNumber());
+        Optional<Patient> patientOptional = findPatientByEmail(email);
+        patientOptional.ifPresent(patientToUpdate -> {
+            patientToUpdate.setPassword(updatedPatient.getPassword());
+            patientToUpdate.setFirstName(patientToUpdate.getFirstName());
+            patientToUpdate.setLastName(updatedPatient.getLastName());
+            patientToUpdate.setPhoneNumber(updatedPatient.getPhoneNumber());
+        });
     }
-    public Patient checkIfPatientExist (String email){
-        Optional<Patient> findPatient = patients.stream()
+
+    private Optional<Patient> findPatientByEmail(String email) {
+        return patients.stream()
                 .filter(patient -> patient.getEmail() != null && patient.getEmail().equals(email))
                 .findFirst();
-        return findPatient.orElse(null);
     }
+
 }
